1a9bca17f0f51e86f07c6850645b73b7c31cc9e0
Bethany Drake <bethanyd@thoughtworks.com>
2018-05-10 10:44:58 +1000

    greeting component

--- SmileyOrReact/App.js
+++ SmileyOrReact/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {Greeting} from './greeting.js'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+
+      <View style ={styles.container}>
+      <View>
+        <Text>Open up App.js to start working on your sapp!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
-    );
+      <Greeting/>
+      </View>
+
+    )
   }
 }
 
---
+++ SmileyOrReact/greeting.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Greeting extends React.Component{
+  render() {
+    return (
+
+      <Text>Hi!</Text>
+
+    );
+  };
+};
